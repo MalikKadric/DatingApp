b5da6b15da7327bd80f503bb2d7ca678b60e3fe6
MalikKadric <malikkadric16@gmail.com>
2021-11-10 01:31:30 +0100

    Angular update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,11 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import  {ToastrModule} from 'ngx-toastr'
 
 
 @NgModule({
@@ -19,6 +23,10 @@ import { RegisterComponent } from './register/register.component';
     NavComponent,
     HomeComponent,
     RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberDetailComponent,
    ],
   imports: [
     BrowserModule,
@@ -26,7 +34,11 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit} from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../models/user';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,29 +10,25 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model: any ={}
-  currentUsers$: Observable<User>
+  model: any = {}
 
-  constructor(public  accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
+
   ngOnInit(): void {
-   this.currentUsers$= this.accountService.currentUser$;
   }
 
- 
-
-  login()
-  {
-    this.accountService.login(this.model).subscribe(responce => {
-      console.log(responce);
-    }, error =>{
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
-   
   }
-  logout(){
-    this.accountService.logout()
 
-    }
-  
-
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/')
+  }
 }
