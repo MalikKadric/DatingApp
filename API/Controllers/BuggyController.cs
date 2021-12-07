cde150df6d4585202a5d1e33d13da338862ca289
MalikKadric <malikkadric16@gmail.com>
2021-12-07 16:01:05 +0100

    End of section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -48,7 +48,7 @@ namespace API.Controllers
         [HttpGet("bad-request")]
         public ActionResult<AppUser> GetBadRequest()
         {
-            return BadRequest("This was not a good request");
+            return BadRequest();
         }
     }
 }
