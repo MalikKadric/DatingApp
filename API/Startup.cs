7b263ce70e1a9319e98afb1fc1a11e91960257f8
MalikKadric <malikkadric16@gmail.com>
2021-12-23 15:44:19 +0100

    Section 18 and 19-pre switch to postgres

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,29 +38,28 @@ namespace API
             services.AddIdentityServices(_config);
             services.AddSignalR();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
                 .WithOrigins("https://localhost:4200"));
-
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
-          
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
